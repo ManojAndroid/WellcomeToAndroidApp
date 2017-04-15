6c5c261a2b66cada43351553d21914f7ee4e4eb7
bridgeit <patil.vaibhav2812@yahoo.com>
2017-04-15 20:03:51 +0530

    new aDDRESSBOOK

---
+++ AddressBook_App/Addressbook_App/src/org/bridgelabz/addressbookApp/Service/AddressBookInterface.java
@@ -0,0 +1,15 @@
+package org.bridgelabz.addressbookApp.Service;
+
+public interface AddressBookInterface {
+	void addPerson();
+
+	void createNewAddressBook();
+
+	void deleteByFirstName();
+
+	void sortByZip();
+
+	void sortByName();
+
+	void editDetails();
+}
